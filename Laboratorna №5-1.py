bff4f3147cd3ba4ac53e56fd9f1058c690d6019c
MuXaKeR9239 <90318926+MuXaKeR9239@users.noreply.github.com>
2021-11-28 19:40:25 +0200

    Update Laboratorna №5-1.py

--- Laboratorna №5-1.py
+++ Laboratorna №5-1.py
@@ -1,59 +1,56 @@
-class Book:
-    def __init__(self, author, title, year, genre):
-        self.author = author
-        self.title = title
-        self.year = year
-        self.genre = genre
-
-class Library:
-    books = []
-    def addBook(self, book):
-        self.books.append(book)
-    
-    def removeBook(self, book):
+class MyLibrary:
+    def __init__(self):
+        self.books = []
+    def add_book(self, author, title, year, genre):
+        self.books.append((author, title, year, genre))
+    def remove_book(self, book):
         self.books.remove(book)
-
-    def searchByAuthor(self, author):
+    def search_author(self, author):
         books = []
         for book in self.books:
-            if book.author == author:
+            if book[0] == author:
                 books.append(book)
         return books
-    
-    def searchByYear(self, year):
+    def search_year(self, year):
         books = []
         for book in self.books:
-            if book.year == year:
+            if book[2] == year:
                 books.append(book)
         return books
-
-    def searchByGenre(self, genre):
+    def search_genre(self, genre):
         books = []
         for book in self.books:
-            if book.genre == genre:
+            if book[3] == genre:
                 books.append(book)
         return books
 
-def main():
-    library = Library()
+lib = MyLibrary()
+lib.add_book(
+    "Анджей Сапковський",
+    "Відьмак",
+    "1990",
+    "Роман"
+)
+lib.add_book(
+    "Дмитро Глуховський",
+    "Метро 2033",
+    "2005",
+    "Роман"
+)
+lib.add_book(
+    "Стівен Кінг",
+    "Сяйво",
+    "1977",
+    "Роман"
+)
 
-    library.addBook(Book('Анджей Сапковський', 'Відьмак', '1990', 'Роман'))
-    library.addBook(Book('Дмитро Глуховський', 'Метро 2033', '2005', 'Роман'))
-    library.addBook(Book('Стівен Кінг', 'Сяйво', '1977', 'Роман'))
+books = lib.search_author("Анджей Сапковський")
+book = books[0]
 
-    author = 'Анджей Сапковський'
-    books = library.searchByAuthor (author)
-    if len(books) == 0:
-        return
-    book = books[0]
+print("Кількість книг автора " + book[0] + ": ", len(books))
+print("Автор книги", book[0])
+print("Титульна сторінка книги" , book[1])
 
-    print('Кількість книг автора'+ author, len(books))
-    print('Автор книги', author)
-    print('Титульна сторінка книги' , book.title)
-
-    library.removeBook(book)
-    books = library.searchByAuthor(author)
-    print('Нова кількість книг автора' + author, len(books))
-
-if __name__ == '__main__':
-    main()    
+lib.remove_book(book)
+books = lib.search_author(book[0])
+print("Нова кількість книг автора " + book[0] + ": ", len(books))
